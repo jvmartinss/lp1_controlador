3b203fa7e3026ec965bb585829d07789bf0d6e52
JOAO VICTOR MARTINS DE ALMEIDA <138832040+jvmartinss@users.noreply.github.com>
2025-01-23 21:52:13 -0300

    Create transporte.h

---
+++ transporte.h
@@ -0,0 +1,36 @@
+#ifndef TRANSPORTE_H
+#define TRANSPORTE_H
+
+#include <string>
+#include <algorithm> 
+#include <cmath> 
+#include "cidade.h"
+
+class Transporte {
+private:
+    std::string nome;
+    char tipo;
+    int capacidade;
+    int velocidade;
+    int distancia_entre_descansos;
+    int tempo_de_descanso;
+    Cidade* localAtual;
+    int distancia_percorrida;
+    int horas_descanso_restantes;
+
+public:
+    Transporte(std::string nome, char tipo, int capacidade, int velocidade, int distancia_entre_descansos, int tempo_de_descanso, Cidade* localAtual);
+
+    std::string getNome() const;
+    char getTipo() const;
+    int getCapacidade() const;
+    Cidade* getLocalAtual() const;
+    void setLocalAtual(Cidade* local);
+
+    bool podeViajar() const;
+    void avancarHoras(int horas);
+    void viajar(int distancia);
+    int calcularTempoViagem(int distancia) const;
+};
+
+#endif
