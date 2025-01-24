2bc267ad5625ae2f2e462731b554d6a302bea978
JOAO VICTOR MARTINS DE ALMEIDA <138832040+jvmartinss@users.noreply.github.com>
2025-01-23 21:48:22 -0300

    Create passageiro.h

---
+++ passageiro.h
@@ -0,0 +1,26 @@
+#ifndef PASSAGEIRO_H
+#define PASSAGEIRO_H
+
+#include <string>
+#include <vector>
+#include "cidade.h"  // Para usar a classe Cidade
+
+class Passageiro {
+public:
+    // Construtor
+    Passageiro(std::string nome, Cidade* localAtual);
+
+    // Métodos para acessar os atributos
+    std::string getNome() const;
+    Cidade* getLocalAtual() const;
+    void setLocalAtual(Cidade* local);
+
+    // Função para salvar os passageiros em um arquivo CSV
+    static void salvarPassageirosEmCSV(const std::vector<Passageiro*>& passageiros);
+
+private:
+    std::string nome;
+    Cidade* localAtual;
+};
+
+#endif // PASSAGEIRO_H
