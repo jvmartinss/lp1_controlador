ffaf8236dbbd450d2d73d3ad4139e28474b31454
JOAO VICTOR MARTINS DE ALMEIDA <138832040+jvmartinss@users.noreply.github.com>
2025-01-23 21:43:00 -0300

    Create cidade.h

---
+++ cidade.h
@@ -0,0 +1,15 @@
+#ifndef CIDADE_H
+#define CIDADE_H
+
+#include <string>
+
+class Cidade {
+private:
+    std::string nome;
+public:
+    Cidade(std::string nome);
+
+    std::string getNome() const;
+};
+
+#endif
